b54636eaf5e98322346e14c77dc40e2f4c5a18fc
vao211 <vaolu211@gmail.com>
2025-10-30 18:22:03 +0700

    A

--- SchoolBookPlatform/Manager/UserManager.cs
+++ SchoolBookPlatform/Manager/UserManager.cs
@@ -18,5 +18,25 @@ namespace SchoolBookPlatform.Manager
             return roleNames;
         }
 
+        public static async Task<bool> IsUserInRoleAsync(this AppDbContext db, Guid userId, string roleName)
+        {
+            var isInRole = await db.UserRoles
+                .Where(ur => ur.UserId == userId)
+                .Join(db.Roles,
+                    ur => ur.RoleId,
+                    r => r.Id,
+                    (ur, r) => r.Name)
+                .AnyAsync(name => name == roleName);
+            return isInRole;
+        }
+
+        public static async Task<string?> GetTrustedDeviceInfoAsync(this AppDbContext db, Guid userId)
+        {
+            var deviceInfo = await db.TrustedDevices
+                .Where(td => td.UserId == userId)
+                .Select(td => td.DeviceInfo)
+                .FirstOrDefaultAsync();
+            return deviceInfo;
+        }
     }
 }
