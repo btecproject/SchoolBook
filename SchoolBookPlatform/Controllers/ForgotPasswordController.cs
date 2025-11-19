469f7c781655c8e433718c82767c5a2603c5f3fd
vao211 <vaolu211@gmail.com>
2025-11-19 12:37:10 +0700

    Chore

--- SchoolBookPlatform/Controllers/ForgotPasswordController.cs
+++ SchoolBookPlatform/Controllers/ForgotPasswordController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using SchoolBookPlatform.Data;
 using SchoolBookPlatform.Manager;
 using SchoolBookPlatform.Services;
