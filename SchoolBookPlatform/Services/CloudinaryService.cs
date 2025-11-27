5398663e0064f79ac2f306886b95e0979b297052
vao211 <vaolu211@gmail.com>
2025-11-27 19:32:54 +0700

    Add: Cloudinary service api

---
+++ SchoolBookPlatform/Services/CloudinaryService.cs
@@ -0,0 +1,15 @@
+﻿using CloudinaryDotNet;
+using SchoolBookPlatform.Data;
+
+namespace SchoolBookPlatform.Services;
+
+public class CloudinaryService(
+    AppDbContext db,
+    Cloudinary cloudinary,
+    ILogger<CloudinaryService> logger
+    )
+{
+    private const string SERVICE_NAME = "cloudinary";
+    
+    
+}
